5484e8ad0e7bcf11c4d91c4966dd1037faa0bece
wukongdaily <2666180@gmail.com>
2024-10-31 20:27:02 +0800

    增加自定义uci脚本和插件

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
 
-
-make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="2048" FILE=immortalwrt-23.05.4-x86-64-generic-squashfs-combined-efi.img.gz
+make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="2048"
