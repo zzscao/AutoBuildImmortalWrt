73562a61e30fd856f57c97dc57a62578955adb09
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-10-31 17:02:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 
 
-make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="2048"
+make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="2048" FILE=immortalwrt-23.05.4-x86-64-generic-squashfs-combined-efi.img.gz
