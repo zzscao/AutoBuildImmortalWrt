36f629b5554cc81c97f42ea7e14ab3e92611a966
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-05 15:01:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,5 +13,7 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+
 
 make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="2048"
