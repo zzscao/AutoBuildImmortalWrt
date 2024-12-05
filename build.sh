29dd4e81973834f9324599d40569ebd379a2322c
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-05 16:23:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,30 +16,25 @@ else
 fi
 
 # 定义所需安装的包列表
-PACKAGES=(
-    curl
-    luci-i18n-firewall-zh-cn
-    luci-i18n-filebrowser-zh-cn
-    luci-app-argon-config
-    luci-i18n-argon-config-zh-cn
-    luci-i18n-opkg-zh-cn
-    luci-i18n-ttyd-zh-cn
-    luci-i18n-passwall-zh-cn
-    luci-app-openclash
-    luci-i18n-homeproxy-zh-cn
-    openssh-sftp-server
-    luci-i18n-dockerman-zh-cn
-)
-
-# 将包列表转换为字符串
-PACKAGE_LIST="${PACKAGES[*]}"
+PACKAGES="\
+curl \
+luci-i18n-firewall-zh-cn \
+luci-i18n-filebrowser-zh-cn \
+luci-app-argon-config \
+luci-i18n-argon-config-zh-cn \
+luci-i18n-opkg-zh-cn \
+luci-i18n-ttyd-zh-cn \
+luci-i18n-passwall-zh-cn \
+luci-app-openclash \
+luci-i18n-homeproxy-zh-cn \
+openssh-sftp-server \
+luci-i18n-dockerman-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
-echo "$PACKAGE_LIST"
+echo "$PACKAGES"
 
-
-make image PROFILE="generic" PACKAGES="$PACKAGE_LIST" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
+make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
