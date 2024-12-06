f4dab6a19af3eb58ed759d9f57954a703bba11c3
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-06 17:09:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,11 +16,18 @@ else
 fi
 
 # 定义所需安装的包列表
-PACKAGES="curl luci-i18n-firewall-zh-cn luci-i18n-filebrowser-zh-cn"
-PACKAGES="$PACKAGES luci-app-argon-config luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn openssh-sftp-server"
+PACKAGES=""
+PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 
 # 构建镜像
