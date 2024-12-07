3e7fbdbb59f092a362eff81437900b1b2ac61b7b
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-07 20:36:42 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -35,7 +35,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="ImmortalWrt 23.05.4 Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by wukongdaily"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
