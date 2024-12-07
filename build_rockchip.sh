ddaf9eca72b599a115741a69d84d8164fae0e5e3
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-07 19:20:54 +0800

    Update build_rockchip.sh

--- build_rockchip.sh
+++ build_rockchip.sh
@@ -3,17 +3,6 @@
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
-# 定义路径变量
-MAKEFILE_PATH="/home/build/immortalwrt/target/linux/rockchip/Makefile"
-
-# 打印 Makefile 内容
-if [ -f "$MAKEFILE_PATH" ]; then
-    echo "Printing Makefile:"
-    cat "$MAKEFILE_PATH"
-else
-    echo "Error: Makefile not found at $MAKEFILE_PATH"
-    exit 1
-fi
 
 # 定义所需安装的包列表
 PACKAGES=""
@@ -22,8 +11,10 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-#PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+#24.10
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+#23.05
+#PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
@@ -35,7 +26,7 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
+make image PROFILE=friendlyarm_nanopi-r3s PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
