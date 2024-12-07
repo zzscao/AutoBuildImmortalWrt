ee50b3d5d866119c5f00093c342144f430f9eefb
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-07 21:38:19 +0800

    Create build.sh

---
+++ rockchip/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 获取 PROFILE 环境变量
+echo "Building for profile: $PROFILE"
+
+
+# 输出调试信息
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
+
+
+# 定义所需安装的包列表
+PACKAGES=""
+PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+#24.10
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+#23.05
+#PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+
+# 构建镜像
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
+echo "$PACKAGES"
+
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
+
+if [ $? -ne 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
+    exit 1
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
