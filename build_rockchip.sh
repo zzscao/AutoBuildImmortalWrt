60995277852ea95551c4f9281b6b719b8408d31d
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2024-12-07 14:26:37 +0800

    Update build_rockchip.sh

--- build_rockchip.sh
+++ build_rockchip.sh
@@ -34,7 +34,7 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
+make image PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="2048"
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
